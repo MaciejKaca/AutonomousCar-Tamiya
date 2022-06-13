622147df7210a9fe1ffa9469b5b18dddb43e173d
MaciejKaca <maciek.kaca@gmail.com>
2022-06-13 09:05:14 -0700

    Added ESC

---
+++ parts/esc.py
@@ -0,0 +1,41 @@
+from utils.conversion import Converter
+from parts.pwm_driver import PWMDriver
+
+class ESCMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class ESC(metaclass=ESCMeta):
+    __pwmDriver = PWMDriver()
+    __ESC_CHANNEL_ID = 0
+
+    __MIN_PWM_VALUE = int(0)
+    __MAX_PWM_VALUE = int(10000)
+
+    __MIN_PWM_DRIVER_VALUE = 0x0000
+    __MAX_PWM_DRIVER_VALUE = 0xFFFF
+
+    __MIN_TARGET_PWM = int(500)
+    __MAX_TARGET_PWM = int(2500)
+
+    MAX_SPEED = int((__MAX_TARGET_PWM-__MIN_TARGET_PWM)/2)
+    MIN_SPEED = int(-MAX_SPEED)
+
+    __ESCChannel = __pwmDriver.getChannel(__ESC_CHANNEL_ID)
+
+    __fromSpeedToPwm = Converter(MIN_SPEED, MAX_SPEED, __MIN_TARGET_PWM, __MAX_TARGET_PWM)
+    __fromPwmToValueDriver = Converter(__MIN_PWM_VALUE, __MAX_PWM_VALUE, __MIN_PWM_DRIVER_VALUE, __MAX_PWM_DRIVER_VALUE)
+
+    def __init__(self):
+        pass
+
+    def setSpeed(self, targetSpeed : int):
+        pwm = self.__fromSpeedToPwm.getTargetValue(targetSpeed)
+        targetValue = self.__fromPwmToValueDriver.getTargetValue(pwm)
+        self.__ESCChannel.duty_cycle = targetValue
--- utils/conversion.py
+++ utils/conversion.py
@@ -3,7 +3,6 @@ class Converter:
     __SOURCE_MIN = int()
     __TARGET_MIN = int()
     __TARGET_MAX = int()
-    __TARGET_MIDDLE = float()
     __SCALE = float()
 
     def __init__(self, source_min : int, source_max : int, target_min : int, target_max : int):
@@ -13,8 +12,7 @@ class Converter:
         self.__TARGET_MIN = target_min
         self.__TARGET_MAX = target_max
 
-        self.__TARGET_MIDDLE = self.__TARGET_MIN + ((self.__TARGET_MAX - self.__TARGET_MIN) / 2)
         self.__SCALE = (self.__TARGET_MAX - self.__TARGET_MIN) / (self.__SOURCE_MAX - self.__SOURCE_MIN)
 
     def getTargetValue(self, number) -> int:
-        return (self.__TARGET_MIDDLE + (number * self.__SCALE))
+        return (self.__TARGET_MAX - ((self.__SOURCE_MAX - number) * self.__SCALE))
