7fb5205693eb9757e9524c433f65c14c6f8a76e8
MaciejKaca <maciek.kaca@gmail.com>
2022-06-23 11:40:39 -0700

    Added client function

--- utils/socket_server.py
+++ utils/socket_server.py
@@ -1,7 +1,8 @@
 import socket
 import queue, threading
+from typing import List
 
-from utils.messages import DataMessage
+from utils.messages import DataMessage, deserialize_message
 
 class CarSocketMeta(type):
     _instances = {}
@@ -14,20 +15,24 @@ class CarSocketMeta(type):
 
 class CarSocket(metaclass=CarSocketMeta):
     def __init__(self) -> None:
+        self.__is_server = True
         self.__localIP     = "192.168.0.115"
         self.__localPort   = 5555
         self.__bufferSize  = 1024
-        self.__UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.__UDPServerSocket.bind((self.__localIP, self.__localPort))
-        self.__client_address = ""
-        self.__UDPServerSocket.settimeout(10)
+        self.__UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.__partner_address = self.__localIP
+        self.__UDPSocket.settimeout(10)
         self.__isConnected = False
-        self.__message_queue = queue.Queue()
+        self.__message_send_queue = queue.Queue()
+        self.__message_recv_queue = queue.Queue()
 
         self.__init_conection()
         
         if self.__isConnected:
-            self.__socket_thread = threading.Thread(target=self.__send, args=(), daemon=True)
+            if self.__is_server:
+                self.__socket_thread = threading.Thread(target=self.__send, args=(), daemon=True)
+            else:
+                self.__socket_thread = threading.Thread(target=self.__recv, args=(), daemon=True)
             self.__socket_thread.start()
 
     def __del__(self):
@@ -37,20 +42,41 @@ class CarSocket(metaclass=CarSocketMeta):
 
     def __init_conection(self):
         try:
-            bytesAddressPair = self.__UDPServerSocket.recvfrom(self.__bufferSize)
-            self.__client_address = bytesAddressPair[1]
-            print("Connected to: ", self.__client_address)
-            self.__isConnected = True
+            if self.__is_server:
+                print("Waiting for client")
+                self.__UDPSocket.bind((self.__localIP, self.__localPort))
+                bytesAddressPair = self.__UDPSocket.recvfrom(self.__bufferSize)
+                self.__partner_address = bytesAddressPair[1]
+                print("Connected to: ", self.__partner_address)
+                self.__isConnected = True
+            else:
+                bytesToSend = str.encode("Hello")
+                self.__UDPSocket.sendto(bytesToSend, (self.__partner_address, self.__localPort))
+                self.__isConnected = True
+            self.__UDPSocket.settimeout(None)
         except socket.timeout as e:
             print("Failed to connect")
             self.__isConnected = False
 
     def add_to_queue(self, message : DataMessage):
         if self.__isConnected:
-            self.__message_queue.put(message.serialize())
+            self.__message_send_queue.put(message.serialize())
+    
+    def get_data(self) -> List[DataMessage]:
+        data_list = []
+        while not self.__message_recv_queue.empty():
+            data  = self.__message_recv_queue.get()
+            data_list.append(data)
+        return data_list
 
     def __send(self):
         while self.__isConnected:
-            message = self.__message_queue.get()
+            message = self.__message_send_queue.get()
             bytesToSend = str.encode(message)
-            self.__UDPServerSocket.sendto(bytesToSend, self.__client_address)
+            self.__UDPSocket.sendto(bytesToSend, self.__partner_address)
+    
+    def __recv(self):
+        while self.__isConnected:
+            message = self.__UDPSocket.recvfrom(self.__bufferSize)
+            data = deserialize_message(message[0])
+            self.__message_recv_queue.put(data)
